f9c82f779f211ee98a6620a3e0119ca158c929b0
xsl <xsl.cmd@gmail.com>
2023-12-27 20:02:21 +0800

    fix

--- llava/model/multimodal_encoder/clip_encoder.py
+++ llava/model/multimodal_encoder/clip_encoder.py
@@ -21,7 +21,8 @@ class CLIPVisionTower(nn.Module):
 
     def load_model(self):
         self.image_processor = CLIPImageProcessor.from_pretrained(self.vision_tower_name)
-        self.vision_tower = CLIPVisionModel.from_pretrained(self.vision_tower_name,cache_dir="/comp_robot/zhanghao/.cache/hugging_face/")
+        # self.vision_tower = CLIPVisionModel.from_pretrained(self.vision_tower_name,cache_dir="./cache/hugging_face/")
+        self.vision_tower = CLIPVisionModel.from_pretrained(self.vision_tower_name)
         self.vision_tower.requires_grad_(False)
 
         self.is_loaded = True
